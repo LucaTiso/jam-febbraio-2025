926bec0fe50d0725da909654658dd7848dad33e3
LucaTiso <luca.dona95mb@gmail.com>
2025-02-11 01:08:30 +0100

    add test code
    add test code

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -20,8 +20,21 @@ public class PlayerController : MonoBehaviour
 
     private float maxSpeedY = 12f;
 
-   
+
     private Vector2 _inputMovement;
+
+
+    // for hard code
+
+    private Vector2 currentMovement;
+
+    private float speedAcceleration = 2f;
+
+    private float turningSpeed = 2f;
+
+    private float maxSpeed = 5f;
+
+    private float speed = 0f;
 
    
 
@@ -29,7 +42,7 @@ public class PlayerController : MonoBehaviour
     {
         _inputMovement = Vector2.zero;
         inputActions = new PlayerInputActions();
-       
+        currentMovement = new Vector2(1, 0);
 
 
 
@@ -77,7 +90,136 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+        easyCode();
+    }
 
+
+    private void hardCode()
+    {
+
+
+        calculateSpeed();
+
+
+        float tmpVelocityX = currentMovement.x;
+
+        float tmpVelocityY = currentMovement.y;
+
+
+        // solo a destra
+        if (_inputMovement.x > 0 && _inputMovement.y==0)
+        {
+
+            if (currentMovement.y > 0 && currentMovement.x >= 0)
+            {
+                tmpVelocityY -= turningSpeed * Time.fixedDeltaTime;
+
+                if (tmpVelocityY < 0)
+                {
+                    tmpVelocityY = 0;
+                }
+
+            }
+            else if (currentMovement.y < 0 && currentMovement.x >= 0)
+            {
+                tmpVelocityY += turningSpeed * Time.fixedDeltaTime;
+
+                if (tmpVelocityY > 0)
+                {
+                    tmpVelocityY = 0;
+                }
+            }else if(currentMovement.y >= 0 && currentMovement.x < 0)
+            {
+
+                tmpVelocityY += turningSpeed * Time.fixedDeltaTime;
+
+                if (tmpVelocityY > 1)
+                {
+                    tmpVelocityY = 1;
+                }
+            }else if(currentMovement.y < 0 && currentMovement.x < 0)
+            {
+                tmpVelocityY -= turningSpeed * Time.fixedDeltaTime;
+                if (tmpVelocityY < -1)
+                {
+                    tmpVelocityY = -1;
+                }
+            }
+
+
+            tmpVelocityX += turningSpeed * Time.fixedDeltaTime;
+
+            if (tmpVelocityX > 1)
+            {
+                tmpVelocityX = 1;
+                tmpVelocityY = 0;
+            }
+
+            
+
+        }
+
+
+
+
+
+
+        currentMovement = new Vector2(tmpVelocityX, tmpVelocityY);
+
+        _rb.velocity = toVelocityVector2(currentMovement.x, currentMovement.y);
+
+
+    }
+
+
+
+
+    private Vector2 toVelocityVector2(float x,float y)
+    {
+
+
+        float magnitude = Mathf.Sqrt(x * x + y * y);
+
+        if (magnitude > 0)
+        {
+            x = (x / magnitude) * speed;
+            y = (y / magnitude) * speed;
+        }
+
+        return new Vector2(x, y);
+    }
+
+
+    private void calculateSpeed()
+    {
+        if(_inputMovement.x!=0 || _inputMovement.y != 0)
+        {
+            speed += speedAcceleration += Time.fixedDeltaTime;
+
+
+            if (speed > maxSpeed)
+            {
+                maxSpeed = speed;
+            }
+
+        }
+        else
+        {
+            speed -= speedAcceleration += Time.fixedDeltaTime;
+
+            if(speed<0)
+            {
+                speed = 0;
+            }
+        }
+
+
+    }
+
+
+
+   private void easyCode()
+    {
         float tmpVelocityY = _rb.velocity.y;
 
 
@@ -92,8 +234,9 @@ public class PlayerController : MonoBehaviour
             {
                 tmpVelocityY += _verticalDeceleration * Time.fixedDeltaTime;
             }
-           
-        }else if (_inputMovement.y<0)
+
+        }
+        else if (_inputMovement.y < 0)
         {
 
             if (tmpVelocityY <= 0)
@@ -104,8 +247,9 @@ public class PlayerController : MonoBehaviour
             {
                 tmpVelocityY -= _verticalDeceleration * Time.fixedDeltaTime;
             }
-            
-        }else 
+
+        }
+        else
         {
 
             if (tmpVelocityY > 0)
@@ -117,7 +261,8 @@ public class PlayerController : MonoBehaviour
                     tmpVelocityY = 0;
                 }
 
-            }else if (tmpVelocityY <0)
+            }
+            else if (tmpVelocityY < 0)
             {
                 tmpVelocityY += _verticalAcceleration * Time.fixedDeltaTime;
 
@@ -135,17 +280,14 @@ public class PlayerController : MonoBehaviour
         if (tmpVelocityY > maxSpeedY)
         {
             tmpVelocityY = maxSpeedY;
-        }else if (tmpVelocityY < -maxSpeedY)
+        }
+        else if (tmpVelocityY < -maxSpeedY)
         {
-            tmpVelocityY= -maxSpeedY;
+            tmpVelocityY = -maxSpeedY;
         }
 
         _rb.velocity = new Vector2(0, tmpVelocityY);
-       
-
     }
 
-
-   
 
 }
